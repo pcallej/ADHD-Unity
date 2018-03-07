3c844ac34498b16017ecba6c7e8622278526030f
Pedro Calle Jaramillo <36971821+pcallej@users.noreply.github.com>
2018-03-06 20:53:09 -0500

    Prueba de repositorio
    Para probar que el repositorio sirve, corre el proyecto en Unity y  dime lo que dice en el log

---
+++ ADHD/Assets/Test.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hola, si puedes leer esto, significa que funcionó");	
+	}
+}
