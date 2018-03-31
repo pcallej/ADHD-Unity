e8fe5833a790c6fde08c0fc2430d56b7751a2482
Pedro Calle Jaramillo <36971821+pcallej@users.noreply.github.com>
2018-03-31 15:29:22 -0500

    Magic to foxes when selected

---
+++ Assets/Scripts/TriggerAnimations.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAnimations : MonoBehaviour {
+
+    private GameObject magic;
+    private Animator anim;
+    public bool magicTrigger;
+
+    public void Start() {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    public void Update()
+    {
+        activateMagic(magicTrigger);
+    }
+
+    public void activateMagic(bool activate) {
+        if (activate) {
+            magic = gameObject.transform.GetChild(4).GetChild(1).gameObject;
+            magic.SetActive(true);
+            Debug.Log("magic activated");
+        }
+    }
+}
