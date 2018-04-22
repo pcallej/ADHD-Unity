8702d34c2099a31a52d21439bccda1f7dabaac47
Pedro Calle Jaramillo <36971821+pcallej@users.noreply.github.com>
2018-04-22 00:33:46 -0500

    Train movement
    move trains vía waypoints

---
+++ Assets/Divided/Scripts/Trains/PathEditor.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class PathEditor : MonoBehaviour {
+
+    private Color trailColor;
+    public List<Transform> pathObjects = new List<Transform>();
+    Transform[] transformArray;
+
+    void OnDrawGizmos()
+    {
+        string trailParent = gameObject.transform.parent.name;
+        switch (trailParent)
+        {
+            case "Red":
+                Gizmos.color = Color.red;
+                break;
+            case "Blue":
+                Gizmos.color = Color.blue;
+                break;
+            case "Green":
+                Gizmos.color = Color.green;
+                break;
+        }
+        transformArray = GetComponentsInChildren<Transform>();
+        pathObjects.Clear();
+
+        foreach (Transform pathObject in transformArray)
+        {
+            if (pathObject != this.transform)
+            {
+                pathObjects.Add(pathObject);
+            }
+
+            for (int i = 0; i < pathObjects.Count; i++)
+            {
+                Vector3 currentPosition = pathObjects[i].position;
+                if (i > 0)
+                {
+                    Vector3 previous = pathObjects[i - 1].position;
+                    Gizmos.DrawLine(previous, currentPosition);
+                    Gizmos.DrawWireSphere(currentPosition,0.3f);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Divided/Scripts/Trains/PathFollower.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFollower : MonoBehaviour {
+
+    public PathEditor pathToFollow;
+    public int currentWayPointID = 0;
+    public float speed;
+    private float reachDistance = 0f;
+    public float rotationSpeed = 5.0f;
+    public string pathName;
+
+    private Vector3 lastPos, currentPos;
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+        //pathToFollow = GameObject.Find(pathName).GetComponent<PathEditor>();
+        lastPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float distance = Vector3.Distance(pathToFollow.pathObjects[currentWayPointID].position,transform.position);
+        transform.position = Vector3.MoveTowards(transform.position,pathToFollow.pathObjects[currentWayPointID].position,Time.deltaTime * speed);
+
+        Quaternion goRotation = Quaternion.LookRotation(pathToFollow.pathObjects[currentWayPointID].position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, goRotation, Time.deltaTime * rotationSpeed);
+
+        if (distance <= reachDistance)
+        {
+            currentWayPointID++;
+        }
+	}
+}
