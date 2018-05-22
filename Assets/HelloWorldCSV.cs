d301a900e5175e113c25efb7050ca3424790565b
Pedro Calle Jaramillo <pcallej@eafit.edu.co>
2018-05-22 11:04:34 -0500

    Sprint 3
    Added menu asnd interfaces

---
+++ Assets/HelloWorldCSV.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Text;
+
+public class HelloWorldCSV : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		string mydocpath = "";
+		
+		if (Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsEditor) {
+            mydocpath = Environment.GetFolderPath (Environment.SpecialFolder.MyDocuments);
+		} else {
+            mydocpath = Environment.GetFolderPath (Environment.SpecialFolder.Personal);
+			mydocpath = mydocpath+"/Downloads/example.csv";
+  		}
+		
+		Debug.Log(mydocpath);
+		//using (TextWriter sw = new StreamWriter("/Users/carlosruiz/Downloads/example.csv"))
+		//using (TextWriter sw = new StreamWriter(mydocpath+"/Downloads/example.csv"))
+		using (TextWriter sw = new StreamWriter(mydocpath))
+            {
+                string strData = "Zorrito";
+                float floatData = 25.63F;//Note it's a float not string
+                sw.WriteLine("{0},{1}", strData, floatData.ToString("F2"));
+            }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Menu/Scripts/LoadMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadMenu : MonoBehaviour {
+
+    [SerializeField] private GameObject targetMenu;
+    [SerializeField] private GameObject menuToDisappear;
+
+    public void TriggerMenu(bool triggermenu) {
+        if (triggermenu) {
+            Debug.Log("activated menu");
+            menuToDisappear.SetActive(false);
+            targetMenu.SetActive(true);
+        }
+    }
+}
